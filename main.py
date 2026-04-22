eeb0af1395fda6bf23189ee6a7859ace2ed12393
PunChen <chainhow@outlook.com>
2026-04-23 01:07:00 +0800

    feat: 接入AstrBot插件-main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger  # 使用 astrbot 提供的 logger 接口
 
-abot = None
+bot = None
 
 
 def process_event(text):
